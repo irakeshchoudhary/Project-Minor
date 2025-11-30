40fa4aa36ba3d2c3a46041baa4c9984aaf29cc37
Rakesh Choudhary <zenerationmedia@gmail.com>
2025-11-30 21:16:23 +0530

    Submit

--- index.js
+++ index.js
@@ -1,89 +1,38 @@
-// 1
-let nums = [10, 20, 30, 40];
-console.log(nums.includes(20));
+const container = document.querySelector('#main');
+const Inputs = document.querySelectorAll('input');
+const Form = document.querySelector('form');
 
-// 2
-let fruits = ["apple", "banana", "orange"];
-console.log(fruits.includes('mongo'));
+Form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-// 3
-let roles = ["user", "editor", "manager"];
-console.log(roles.includes('admin'));
+    const Card = document.createElement('div')
+    Card.classList.add('Card');
 
-// 4
-let skills = ["HTML", "CSS", "React"];
+    const Profile = document.createElement('Profile');
+    Profile.classList.add('Profile');
 
-if (skills.includes('javascript')) {
-    console.log('Found');
-}
-else {
-    console.log('Not Found');
-}
+    const img = document.createElement('img');
+    img.setAttribute('src', Inputs[3].value)
 
-// 5
-let prices = [200, 300, 400];
+    const Name = document.createElement('h2');
+    Name.textContent = Inputs[0].value;
+    const Email = document.createElement('h6');
+    Email.textContent = Inputs[2].value;
+    const Age = document.createElement('p');
+    Age.textContent = Inputs[1].value;
 
-if (!prices.includes(100)) {
-    prices.push(100);
-}
+    Profile.appendChild(img);
+    Card.appendChild(Profile);
+    Card.appendChild(Name);
+    Card.appendChild(Age);
+    Card.appendChild(Email);
 
-console.log(prices);
+    container.appendChild(Card);
 
+    Inputs.forEach(function (del){
+        if(del.type !== 'submit'){
+            del.value = '';
+        }
 
-// 6
-let studentList = ["Amit", "Vivek", "Soham"];
-
-if (studentList.includes('Rakesh')) {
-    console.log('Present');
-}
-else {
-    console.log('Absent');
-}
-
-
-// 7
-let numbers = [11, 21, 33, 55];
-
-const Arr = () => {
-    if(numbers%2 === 0){
-        console.log(numbers);
-    }
-    else{
-        console.log('No Even Numbers Available');
-    }
-}
-
-Arr();
-
-// 8
-let fruits1 = ["Mango", "Banana", "APPLE"];
-
-if(fruits1.includes('APPLE')){
-    let index =  fruits1.indexOf('APPLE');
-    fruits1[index] = fruits1[index].toLowerCase();
-}
-
-console.log(fruits1)
-
-// 9
-let letters = ["A", "B", "C", "D"];
-
-console.log(letters.includes('A' || 'a' && 'C' || 'c'));
-
-// 10
-let cart = ["shoes", "bag"];
-
-function checkProduct(cart, item) {
-    if (cart.includes(item)) {
-        console.log("Already in cart");
-    } else {
-        cart.push(item);
-        console.log("Added");
-    }
-}
-
-checkProduct(cart, "Iphone");
-console.log(cart);
-
-
- 
+    })
+})
